44614a0aebe58f6fdcc74b2b6904ec76c5b26c55
Msaid <407795@niuitmo.ru>
2026-03-18 00:03:26 +0300

    save file

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
+import os
+import shutil
+import random
+import datetime
 import uvicorn
-from fastapi import FastAPI, Request, status, File, UploadFile
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
-from fastapi.exceptions import RequestValidationError
+from fastapi import FastAPI, Request, HTTPException, status, File, UploadFile
 
 app = FastAPI()
-
 @app.post("/public/report/export")
 def upload_file(file: UploadFile):
     if file.content_type != "text/plain":
-        return JSONResponse(
-            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
-            content=jsonable_encoder({"detail": "Invalid file: send .txt file"})
-        )
-    return {"filename": file.filename, "content_type": file.content_type}
+        raise HTTPException(422, {"detail": "Invalid file: send .txt file"})
+    
+    rand_file = random.randint(1, 1000000)
+
+    path_name = f"files/{rand_file}_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+    os.makedirs(path_name, exist_ok=True)
+    file_path = f"{path_name}/{rand_file}_{file.filename}"
+    with open(file_path, "wb+") as file_content:
+        shutil.copyfileobj(file.file, file_content)
+    return {"filename": file.filename, "content_type": file.content_type, "file_path": file_path}
 
     
 

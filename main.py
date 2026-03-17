7834f1b50405f69044953430ffe709f300d1ab09
Msaid <407795@niuitmo.ru>
2026-03-17 22:50:47 +0300

    first route

---
+++ main.py
@@ -0,0 +1,12 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.post("/public/report/export")
+def read_root():
+    return {"Hello": "World"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)

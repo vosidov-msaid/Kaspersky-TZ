237b1a9fa3a721f3e0a39c45faec8bd03f19f646
Msaid <407795@niuitmo.ru>
2026-03-17 23:08:27 +0300

    process valid file

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status, File, UploadFile
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
 
 app = FastAPI()
 
-
 @app.post("/public/report/export")
-def read_root():
-    return {"Hello": "World"}
+def upload_file(file: UploadFile):
+    if file.content_type != "text/plain":
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+            content=jsonable_encoder({"detail": "Invalid file: send .txt file"})
+        )
+    return {"filename": file.filename, "content_type": file.content_type}
+
+    
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
